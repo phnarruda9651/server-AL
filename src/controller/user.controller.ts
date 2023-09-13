15389569ef6640af3838e7281a6530841893ffd4
Tacianojr07 <t.juniior13@gmail.com>
2023-09-13 10:35:07 -0300

    adding encryption to password

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,8 +1,11 @@
+import bcrypt from 'bcrypt';
 import { createUser } from "../repository/user.repository";
 import { Request, Response } from "express";
 
 export const create = async(req: Request, res: Response)=> {
     try {
+        const hashPassword = await bcrypt.hash(req.body.password, 10)
+        req.body.password = hashPassword
         const user = await createUser(req.body)
         res.status(200).send(user);
     } catch (error) {
