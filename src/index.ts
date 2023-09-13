3ac934ad6ba423cbbcaca0b93412c60e0009cee7
Tacianojr07 <t.juniior13@gmail.com>
2023-09-13 10:03:06 -0300

    CRUD method: post finish

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv';
+import routes from './routes';
 
 dotenv.config()
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+routes(app);
+
 
 app.listen(3000);
 console.log("servidor running")
