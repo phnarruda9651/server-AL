a9dbd154cd3b56544b449d35c7a065d2c2dd6821
Tacianojr07 <t.juniior13@gmail.com>
2023-09-14 12:10:12 -0300

    authenticate is working

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+declare global {
+    namespace Express {
+      interface Request {
+        userId: string;
+      }
+    }
+  }
+
+type TokenPayLoad = {
+  id: string;
+  iat: number;
+  exp: number;
+};
+
+export function AuthMiddlwares(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  const [, token] = authorization.split(" ");
+
+  try {
+    const decoded = verify(token, "secret");
+    const { id } = decoded as TokenPayLoad;
+
+    req.userId = id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Token invalid" });
+  }
+}
